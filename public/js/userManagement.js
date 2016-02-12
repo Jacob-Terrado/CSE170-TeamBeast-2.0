08796c29b2298cc45b4834075ea151627fa8e459
Jacob Terrado <jsterrad@ucsd.edu>
2016-02-11 22:10:52 -0800

    Added user management and parse login

--- public/js/userManagement.js
+++ public/js/userManagement.js
@@ -4,44 +4,59 @@
 Parse.initialize("gxu4L1e0XELkQTfADYQgfLtFHIov0P1TWcKh7KmV", "KuGlPT84K0fhs3Dwt3jCrFUbVVSxFoycOUCBvF8N");
 
 function automaticLogin() {
-	if (Parse.User.currentUser()) {
-		// user is logged in
-	} else {
-		// user is not logged in
-	}
+	console.log("automaticLogin");
+	// if (Parse.User.currentUser()) {
+	// 	// user is logged in
+	// } else {
+	// 	// user is not logged in
+	// }
 };
 
 function signUp() {
-	// Grab values from DOM
-  var person = new Parse.User();
-  
-  person.set("username", username);
-  person.set("password", password);
-  person.set("email", email);
-  person.set("birthdate", birthdate);
-  person.set("firstName", firstName);
-  person.set("lastName", lastName);
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var username = document.getElementById("username").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
 
-  person.signUp(null, {success: function(user){
-      console.log("Sign up Success");
-  }, error: function(user, error){
-      console.log(error);
-  }});
+	if (firstName && lastName && username && email && password) {
+	  var person = new Parse.User();
+	  person.set("username", username);
+	  person.set("password", password);
+	  person.set("email", email);
+	  person.set("firstName", firstName);
+	  person.set("lastName", lastName);
+
+	  person.signUp(null, {success: function(user){
+	  		window.location = "http://localhost:3000/profile"
+	  		//window.location = "http://friendler.herokuapp.com/profile"
+	      console.log("Sign up Success");
+	  }, error: function(user, error){
+	      console.log(error);
+	  }});
+	}
   
    event.preventDefault();
 };
 
 function login() {
-	// Grab username and password from DOM
+	var username = document.getElementById("usernameLogin").value;
+	var password = document.getElementById("passwordLogin").value;
 
-	Parse.User.logIn(username, password, {
-	  success: function(user) {
-	    // Do stuff after successful login.
-	  },
-	  error: function(user, error) {
-	    // The login failed. Check error to see why.
-	  }
-	});
+	if (username && password) {
+		Parse.User.logIn(username, password, {
+		  success: function(user) {
+		    // Do stuff after successful login.
+		    window.location = "http://localhost:3000/profile"
+		    // window.location = "http://friendler.herokuapp.com/profile"
+		    console.log("successfull");
+		  },
+		  error: function(user, error) {
+		    // The login failed. Check error to see why.
+		    console.log("unsuccessfull");
+		  }
+		});
+	}
 };
 
 function logout() {
