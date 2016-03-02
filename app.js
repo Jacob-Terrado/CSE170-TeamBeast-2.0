91e1cb3a60c1cf25eff316e6a0245e0a99c4b170
Jacob Terrado <jsterrad@ucsd.edu>
2016-03-01 16:27:56 -0800

    Added google analytics for A/B testing but still need to test if it works

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get('/', index.viewIndex);
 app.get('/store', store.viewStore);
 app.get('/inventory', inventory.viewInventory);
 app.get('/tutorial', tutorial.viewTutorial);
+app.get('/tutorial2', tutorial.viewTutorial2);
 app.get('/profile', profile.viewProfile);
 app.get('/friends', friends.viewFriends);
 app.get('/checkin', checkin.viewCheckIn);
--- routes/tutorial.js
+++ routes/tutorial.js
@@ -1,3 +1,7 @@
-exports.viewTutorial = function(req, res){
-	res.render("tutorial");
+exports.viewTutorial = function (req, res) {
+    res.render("tutorial");
+};
+
+exports.viewTutorial2 = function (req, res) {
+    res.render("tutorial2");
 };
